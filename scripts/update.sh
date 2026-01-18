6057589220a59ceef9ddc50d627b2ac473cfb0f3
Renier John Sediego <renierjohnsediego@gmail.com>
2026-01-18 11:18:34 +0800

    Add more scripts.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bash scripts/composer.sh install
-bash scripts/drush updb -y
-bash scripts/drush cim -y
-bash scripts/drush cr
-bash scripts/drush uli
+bash scripts/drush.sh updb -y
+bash scripts/drush.sh cim -y
+bash scripts/drush.sh cr
+bash scripts/drush.sh uli
